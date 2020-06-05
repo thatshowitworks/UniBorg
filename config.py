579214108ed82df655637bbe0c73ff5d961a0ac8
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-06-05 22:28:47 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+from sample_config import Config
+
+class Development(Config):
+  APP_ID = 928403
+  API_HASH = "4a546388b91f6f815c4a6adbbc30d574"
+  NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD = 4
+  GOOGLE_SEARCH_COUNT_LIMIT = 5
+  PRIVATE_GROUP_BOT_API_ID = -1001107877043
+  DB_URI = "postgres://baocqbagfytzub:be072b56e4f7146c970611961ceca06c6a553f279718abd2f2c07c586e6e2cbd@ec2-54-163-226-30.compute-1.amazonaws.com:5432/d15a7uls1e0ebj"
+  TG_BOT_TOKEN_BF_HER = "812423101:AAEpBE5RNIZ8zaBOJN7SD31rs1VO6dz9zNc"
+  TG_BOT_USER_NAME_BF_HER = "satwik_ka_bot"
+  TMP_DOWNLOAD_DIRECTORY = "./DOWNLOADS/"
+  G_DRIVE_CLIENT_ID = "91901208556-re664n44vqmj3itptrjq36spgfmbt5l7.apps.googleusercontent.com"
+  G_DRIVE_CLIENT_SECRET = "l4YfrlnXHBW3Py0XHz9mZv1Q"
+  AUTH_TOKEN_DATA = ""
+  # specify LOAD and NO_LOAD
+  LOAD = []
+  NO_LOAD = []
