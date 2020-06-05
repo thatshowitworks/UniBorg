f881015911f3f32acb088702cd93d511415405ae
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-06-05 22:36:47 +0530

    Update google.py

--- stdplugins/google.py
+++ stdplugins/google.py
@@ -1,15 +1,17 @@
 """ Powered by @Google
 Available Commands:
-.google search <query>
-.google image <query>
-.google reverse search"""
+.gs <query>
+.gi <query>
+.grs"""
 
 import asyncio
 import os
+import shutil
+from re import findall
+from google_img import googleimagesdownload
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-from google_images_download import google_images_download
 from uniborg.util import admin_cmd
 
 
@@ -17,7 +19,7 @@ def progress(current, total):
     logger.info("Downloaded {} of {}\nCompleted {}".format(current, total, (current / total) * 100))
 
 
-@borg.on(admin_cmd(pattern="google search (.*)"))
+@borg.on(admin_cmd(pattern="gs (.*)"))
 async def _(event):
     if event.fwd_from:
         return
@@ -42,48 +44,44 @@ async def _(event):
     await event.edit("Google: {}\n{}".format(input_str, output_str), link_preview=False)
 
 
-@borg.on(admin_cmd(pattern="google image (.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    start = datetime.now()
-    await event.edit("Processing ...")
-    input_str = event.pattern_match.group(1)
-    response = google_images_download.googleimagesdownload()
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+@borg.on(admin_cmd(pattern="gi ?(.*)"))
+async def img_sampler(event):
+    await event.edit("`Processing...`")
+    reply = await event.get_reply_message()
+    if event.pattern_match.group(1):
+        query = event.pattern_match.group(1)
+    elif reply:
+        query = reply.message
+    else:
+    	await event.edit("`um, mind mentioning what I actually need to search for ;_;`")
+    	return
+        
+    lim = findall(r"lim=\d+", query)
+    # lim = event.pattern_match.group(1)
+    try:
+        lim = lim[0]
+        lim = lim.replace("lim=", "")
+        query = query.replace("lim=" + lim[0], "")
+    except IndexError:
+        lim = 5
+    response = googleimagesdownload()
+
+    # creating list of arguments
     arguments = {
-        "keywords": input_str,
-        "limit": Config.TG_GLOBAL_ALBUM_LIMIT,
+        "keywords": query,
+        "limit": lim,
         "format": "jpg",
-        "delay": 1,
-        "safe_search": True,
-        "output_directory": Config.TMP_DOWNLOAD_DIRECTORY
+        "no_directory": "no_directory"
     }
+
+    # passing the arguments to the function
     paths = response.download(arguments)
-    logger.info(paths)
-    lst = paths[0].get(input_str)
-    if len(lst) == 0:
-        await event.delete()
-        return
-    await borg.send_file(
-        event.chat_id,
-        lst,
-        caption=input_str,
-        reply_to=event.message.id,
-        progress_callback=progress
-    )
-    logger.info(lst)
-    for each_file in lst:
-        os.remove(each_file)
-    end = datetime.now()
-    ms = (end - start).seconds
-    await event.edit("searched Google for {} in {} seconds.".format(input_str, ms), link_preview=False)
-    await asyncio.sleep(5)
+    lst = paths[0][query]
+    await event.client.send_file(await event.client.get_input_entity(event.chat_id), lst)
+    shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
     await event.delete()
 
-
-@borg.on(admin_cmd(pattern="google reverse search"))
+@borg.on(admin_cmd(pattern="grs"))
 async def _(event):
     if event.fwd_from:
         return
