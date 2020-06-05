f7fb795f0505982e5df0044cc73699c82f2fba80
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-06-05 23:12:24 +0530

    Update gDrive.py

--- stdplugins/gDrive.py
+++ stdplugins/gDrive.py
@@ -36,7 +36,7 @@ OAUTH_SCOPE = "https://www.googleapis.com/auth/drive.file"
 # Redirect URI for installed apps, can be left as is
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 # global variable to set Folder ID to upload to
-G_DRIVE_F_PARENT_ID = None
+G_DRIVE_F_PARENT_ID = "1CMHJAULGWaE6IGTjtdLvGozV5m5k96vo"
 # global variable to indicate mimeType of directories in gDrive
 G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
 
