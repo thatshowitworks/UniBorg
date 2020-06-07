45d218b1667739bad8ebb8f04d4092b843b79f4d
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-06-07 22:21:49 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -108,7 +108,7 @@ class Config(object):
     # define the "types" that should be uplaoded as streamable
     TL_VID_STREAM_TYPES = ("MP4", "WEBM")
     TL_MUS_STREAM_TYPES = ("MP3", "WAV", "FLAC")
-
+    DEEZER_ARL_TOKEN = os.environ.get("DEEZER_ARL_TOKEN", None)
 
 class Production(Config):
     LOGGER = False
