2e4e511374ae9041046b39321476a482de0ab100
thatshowitworks <62587456+thatshowitworks@users.noreply.github.com>
2020-07-16 20:58:01 +0530

    Update get_id.py

--- stdplugins/get_id.py
+++ stdplugins/get_id.py
@@ -5,7 +5,7 @@ from telethon.utils import pack_bot_file_id
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="get_id"))
+@borg.on(events.NewMessage(pattern='.get_id'))
 async def _(event):
     if event.fwd_from:
         return
