ffde96e9c6299f0dbf770044b18a82cf3e0c7e45
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-08-10 09:20:55 +0530

    Update music.py

--- stdplugins/music.py
+++ stdplugins/music.py
@@ -7,7 +7,7 @@ import asyncio
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("music (.*)"))
+@borg.on(admin_cmd(pattern="music ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
